07cbed80bfb1663d6ac90b686323dff72bcd4a5f
Michael Barker <mikeb01@gmail.com>
2017-03-14 15:06:11 +1300

    Test for queries spanning multiple blocks

---
+++ src/main/java/com/lmax/collection/Maps.java
@@ -0,0 +1,19 @@
+package com.lmax.collection;
+
+import java.util.Map;
+import java.util.function.BiConsumer;
+import java.util.function.Supplier;
+
+public class Maps
+{
+    public static <K, V, T> Map<K, V> mapOf(Iterable<T> source, BiConsumer<T, Map<K, V>> transform, Supplier<Map<K, V>> mapSupplier)
+    {
+        Map<K, V> map = mapSupplier.get();
+        for (T t : source)
+        {
+            transform.accept(t, map);
+        }
+
+        return map;
+    }
+}
--- src/test/java/com/lmax/utan/store/ValueDataSeriesTest.java
+++ src/test/java/com/lmax/utan/store/ValueDataSeriesTest.java
@@ -1,17 +1,21 @@
 package com.lmax.utan.store;
 
+import com.lmax.collection.Maps;
 import org.assertj.core.api.Assertions;
 import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Random;
 
 import static com.lmax.utan.store.Block.new4kHeapBlock;
 import static com.lmax.utan.store.BlockGenerator.generateBlockData;
 import static java.lang.Math.abs;
+import static java.util.Arrays.asList;
 import static java.util.Collections.singletonList;
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -23,17 +27,38 @@ public class ValueDataSeriesTest
     @Test
     public void returnValuesWithinSingleBlock() throws Exception
     {
-        final Block b = new4kHeapBlock();
         final List<Entry> entries = new ArrayList<>();
+        final Block b = new4kHeapBlock();
+
         final long startTimestamp = abs(random.nextLong());
         final long endTimestamp = generateBlockData(startTimestamp, random, b, entries);
 
-        BlockTest.assertTimestampsAndValues(b, entries);
+        assertQuery(entries, startTimestamp, endTimestamp, singletonList(b));
+    }
 
-        valueDataSeries.load(n -> singletonList(b));
+    @Test
+    public void returnValuesSpanningTwoBlocks() throws Exception
+    {
+        final List<Entry> entries = new ArrayList<>();
+        final Block b1 = new4kHeapBlock();
+        final Block b2 = new4kHeapBlock();
 
-        long queryStart = startTimestamp + 10_000;
+        final long beginTimestamp1 = abs(random.nextLong());
+        final long endTimestamp1 = generateBlockData(beginTimestamp1, random, b1, entries);
+        final long endTimestamp2 = generateBlockData(endTimestamp1 + 1000, random, b1, entries);
+
+        assertQuery(entries, beginTimestamp1, endTimestamp2, asList(b1, b2));
+    }
+
+    private void assertQuery(List<Entry> entries, long beginTimestamp, long endTimestamp, List<Block> blocks)
+    {
+        Map<Long, Double> timestampToValue = Maps.mapOf(entries, (e, m) -> m.put(e.timestamp, e.value), HashMap::new);
+
+        valueDataSeries.load(n -> blocks);
+
+        long queryStart = beginTimestamp + 10_000;
         long queryEnd = endTimestamp - 10_000;
+
         int[] count = { 0 };
 
         valueDataSeries.query(
@@ -43,6 +68,9 @@ public class ValueDataSeriesTest
             {
                 assertThat(k).isGreaterThanOrEqualTo(queryStart);
                 assertThat(k).isLessThan(queryEnd);
+
+                assertThat(v).isEqualTo(timestampToValue.get(k));
+
                 count[0]++;
             });
 
